f87dc633fb4fc244fe42b9f542abedf1e86dcd80
PracticeKJY <poiijtl1004@naver.com>
2023-04-25 19:58:46 +0900

    learn. part1-9까지 학습완료
    - 1-7~1-9 학습완료
    - 배운개념
       - state/props
       - useState/onClick
       - default value를 array로 했을 때의 useState 사용법

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # [Next.js로 웹서비스 만들기](https://codingapple.com/course/next-js/)
 
+## Part1
+
 - [x] 1. 설치
 
 - [x] 2. react 기본 문법
@@ -15,3 +17,9 @@
 - [x] 5. <IMAGE> 태그를 통한 image 넣기
 
 - [x] 6. component
+
+- [x] 7. Component에 데이터 전해주려면 props (state/props)
+
+- [x] 8. 좋아요 버튼 만들기 (useState,onClick)
+
+- [x] 9. 좋아요 버튼 만들기 (array,object state 변경하려면)
--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -3,20 +3,48 @@ import Image, { StaticImageData } from "next/image"
 import coconut from "/public/coconut.png"
 import pasta from "/public/pasta.png"
 import tomato from "/public/tomato.png"
+import { FC, useState } from "react"
+interface Product {
+  name: string
+  price: number
+  src: string | StaticImageData
+  alt: string
+  quantity: number
+  count: number
+}
 
 const List = () => {
-  interface Product {
-    name: string
-    price: number
-    src: string | StaticImageData
-    alt: string
-  }
-
   const products: Product[] = [
-    { name: "토마토", price: 7, src: "/tomato.png", alt: "토마토" },
-    { name: "파스타", price: 12, src: "/pasta.png", alt: "파스타" },
-    { name: "코코넛", price: 5, src: coconut, alt: "코코넛" },
+    {
+      name: "토마토",
+      price: 7,
+      src: "/tomato.png",
+      alt: "토마토",
+      quantity: 100,
+      count: 0,
+    },
+    {
+      name: "파스타",
+      price: 12,
+      src: "/pasta.png",
+      alt: "파스타",
+      quantity: 100,
+      count: 0,
+    },
+    {
+      name: "코코넛",
+      price: 5,
+      src: coconut,
+      alt: "코코넛",
+      quantity: 100,
+      count: 0,
+    },
   ]
+
+  const [장바구니수량, 장바구니수량변경] = useState(0)
+  const [담긴수량, 담긴수량변경] = useState([0, 0, 0])
+
+  console.log(담긴수량)
 
   return (
     <div>
@@ -28,6 +56,13 @@ const List = () => {
             <Image width="200" height="200" src={product.src} alt="" />
             <h4 className={styles.productName}>{product.name}</h4>
             <p className={styles.productPrice}>${product.price}</p>
+            <div className={styles.counter}>
+              <Counter
+                index={index}
+                담긴수량={담긴수량}
+                담긴수량변경={담긴수량변경}
+              />
+            </div>
           </div>
         )
       })}
@@ -36,3 +71,33 @@ const List = () => {
 }
 
 export default List
+
+interface ICounter {
+  index: number
+  담긴수량: number[]
+  담긴수량변경: (담긴수량: number[]) => void
+}
+
+export const Counter: FC<ICounter> = ({ index, 담긴수량, 담긴수량변경 }) => {
+  const plus = (index: number) => {
+    const copy담긴수량 = [...담긴수량]
+    copy담긴수량[index]++
+    담긴수량변경(copy담긴수량)
+  }
+  const minus = (index: number) => {
+    if (담긴수량[index] < 1) {
+      return
+    }
+    const new담긴수량 = [...담긴수량]
+    new담긴수량[index]--
+    담긴수량변경(new담긴수량)
+  }
+
+  return (
+    <>
+      <button onClick={() => minus(index)}>-</button>
+      <span>{담긴수량[index]}</span>
+      <button onClick={() => plus(index)}>+</button>
+    </>
+  )
+}
